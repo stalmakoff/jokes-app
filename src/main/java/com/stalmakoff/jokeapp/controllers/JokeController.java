ffcd431c997ed2787c7203fff6cc00eac82e51d6
Daniel <stalmakoff@protonmail.com>
2020-02-09 03:36:01 +0200

    Creating controller with logic

---
+++ src/main/java/com/stalmakoff/jokeapp/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package com.stalmakoff.jokeapp.controllers;
+
+import com.stalmakoff.jokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+
+    }
+
+}
